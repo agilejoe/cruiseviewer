15367e3ea57fddc2cb38114d8cb14e79e5326f67
Jason Meridth <jmeridth@gmail.com>
2009-12-31 14:03:31 -0600

    add loading text.  change text color outside success or fail divs.

--- js/cruiseviewer.js
+++ js/cruiseviewer.js
@@ -4,12 +4,12 @@ $(document).ready(function() {
       url: "http://cruisecontrolrb.thoughtworks.com/XmlStatusReport.aspx",
       dataType: "html",
       success: function(xml) { 
-        $("#parentdiv").empty()
+        $("#parentDiv").empty();
         $(xml).find("Project").each(function()
         {
           var name = $(this).attr("name");
           var buildStatus = $(this).attr("lastBuildStatus");
-          $("<div class='"+buildStatus+"'>"+name+"</div>").hide().appendTo("#parentDiv").fadeIn();
+          $("<div class='"+buildStatus+"'>"+name+"</div>").appendTo("#parentDiv");
         });
       }
     });
